8f2557bf74930973c2e8bd1307bb3ca8f863a23e
George Nychis <gnychis@gmail.com>
2011-03-14 15:46:57 -0400

    fixing up build to handle wlan driver

--- scripts/coexisyst_pack.sh
+++ scripts/coexisyst_pack.sh
@@ -4,6 +4,10 @@ mkdir -p coexisyst_build/kernel
 mkdir -p coexisyst_build/system/lib/modules
 find . -name zImage -exec cp {} coexisyst_build/kernel/ \; &> /dev/null
 find . -name "*.ko" -exec cp {} coexisyst_build/system/lib/modules/ \; &> /dev/null
+cdir=$(pwd)
+cd ../wlan/wl1271/platforms/os/linux
+cd $cdir
+cp ../wlan/wl1271/platforms/os/linux/tiwlan_drv.ko coexisyst_build/system/lib/modules/
 cd coexisyst_build
 tar cf ../kernel.tar *
 cd ../
