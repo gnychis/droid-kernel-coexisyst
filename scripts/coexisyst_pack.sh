afd54551c3f8aa1b0d6f9a9b7b34e32d72d86b31
George Nychis <gnychis@gmail.com>
2011-03-14 16:06:01 -0400

    fixing build

--- scripts/coexisyst_pack.sh
+++ scripts/coexisyst_pack.sh
@@ -6,6 +6,7 @@ find . -name zImage -exec cp {} coexisyst_build/kernel/ \; &> /dev/null
 find . -name "*.ko" -exec cp {} coexisyst_build/system/lib/modules/ \; &> /dev/null
 cdir=$(pwd)
 cd ../wlan/wl1271/platforms/os/linux
+make
 cd $cdir
 cp ../wlan/wl1271/platforms/os/linux/tiwlan_drv.ko coexisyst_build/system/lib/modules/
 cd coexisyst_build
